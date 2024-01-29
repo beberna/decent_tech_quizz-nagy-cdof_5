bcc37d66719c2bfbcf53e25a203e29ea33b0b42e
beberna <153800485+beberna@users.noreply.github.com>
2024-01-29 17:24:07 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='decent_tech_quizz-nagy-codof_5',
+ version='1.0',
+ author='beberna',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
